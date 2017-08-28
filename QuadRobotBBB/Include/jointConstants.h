eebf870c31389250da234cb2a21f373b8e859f1a
acardi <acardi@debian.AAA>
2017-08-27 22:15:27 -0400

    Got angle readout working on two encoders

--- QuadRobotBBB/Include/jointConstants.h
+++ QuadRobotBBB/Include/jointConstants.h
@@ -4,33 +4,33 @@
 #define DEG_PER_BIT 0.087890625f // Number of degrees per bit of a 4096 count encoder
 
 const unsigned short int minEncRead[20] = {
-		100, 100, 100, 100, 100, // Leg 1
+		100, 100, 100, 1230, 1420, // Leg 1
 		100, 100, 100, 100, 100, // Leg 2
 		100, 100, 100, 100, 100, // Leg 3
 		100, 100, 100, 100, 100 // Leg 4
 };
 
 const unsigned short int maxEncRead[20] = {
-		2000, 2000, 2000, 2000, 2000, // Leg 1
+		2000, 2000, 2000, 2386, 2454, // Leg 1
 		2000, 2000, 2000, 2000, 2000, // Leg 2
 		2000, 2000, 2000, 2000, 2000, // Leg 3
 		2000, 2000, 2000, 2000, 2000 // Leg 4
 };
 
 const float angleAtMinEncoderReading[20] = {
-		30.6, 30.6, 30.6, 30.6, 30.6, // Leg 1
-		30.6, 30.6, 30.6, 30.6, 30.6, // Leg 2
-		30.6, 30.6, 30.6, 30.6, 30.6, // Leg 3
-		30.6, 30.6, 30.6, 30.6, 30.6 // Leg 4
+		30.6f, 30.6f, 30.6f, -45.0f, -45.0f, // Leg 1
+		30.6f, 30.6f, 30.6f, 30.6f, 30.6f, // Leg 2
+		30.6f, 30.6f, 30.6f, 30.6f, 30.6f, // Leg 3
+		30.6f, 30.6f, 30.6f, 30.6f, 30.6f // Leg 4
 };
 
-// This is the direction of the chanmge in encoder counts with increasing angle
-// A positive number indicates that an inrease in encoder count is a positive change in joint angle
+// This is the direction of the change in encoder counts with increasing angle
+// A positive number indicates that an increase in encoder count is a positive change in joint angle
 const float encDir[20] = {
-		1.0f,1.0f,1.0f,1.0f,-1.0f, // Leg 1
-		1.0f,1.0f,1.0f,1.0f,-1.0f, // Leg 2
-		1.0f,1.0f,1.0f,1.0f,-1.0f, // Leg 3
-		1.0f,1.0f,1.0f,1.0f,-1.0f  // Leg 4
+		1.0f, 1.0f, 1.0f, 1.0f, 1.0f, // Leg 1
+		1.0f, 1.0f, 1.0f, 1.0f, 1.0f, // Leg 2
+		1.0f, 1.0f, 1.0f, 1.0f, 1.0f, // Leg 3
+		1.0f, 1.0f, 1.0f, 1.0f, 1.0f  // Leg 4
 };
 
 #endif /* JOINTCONSTANTS_H_ */
--- QuadRobotBBB/src/robotMotion.cpp
+++ QuadRobotBBB/src/robotMotion.cpp
@@ -96,7 +96,7 @@ void getJointAngles(unsigned char *forwardV, unsigned char *rotationV, float *p_
 
 void getMotorCommands(unsigned char *p_motorCommand, float *p_desiredAngle, struct LEG_PCB *p_LEGdata)
 {
-	/*float currentJointAngle[NUM_ENCODERS*NUM_LEGS];
+	float currentJointAngle[NUM_ENCODERS*NUM_LEGS];
 
 	unsigned char OL, IL;
 
@@ -108,74 +108,79 @@ void getMotorCommands(unsigned char *p_motorCommand, float *p_desiredAngle, stru
 					(float)(p_LEGdata[OL].encoder[IL] - minEncRead[OL*NUM_ENCODERS+IL])*DEG_PER_BIT*encDir[OL*NUM_ENCODERS+IL] + angleAtMinEncoderReading[OL*NUM_ENCODERS+IL];
 
 			//cout << (int)(OL*NUM_ENCODERS+IL) << ")" << (float)currentJointAngle[OL*NUM_ENCODERS+IL] << endl;
-			printf("Angle %d) %f\n",OL*NUM_ENCODERS+IL,currentJointAngle[OL*NUM_ENCODERS+IL]);
+
 		}
-	}*/
-		unsigned char check1Tx, check2Tx;
-		unsigned short int i;
-		unsigned short int sum1Tx, sum2Tx;
+	}
 
-		unsigned char high = 20, low = 40;
+	printf("Angle %d) %f\n",4,currentJointAngle[3]);
+	printf("Angle %d) %f\n",5,currentJointAngle[4]);
 
-		// The first four bytes are preamble bytes
-		for (i = 0; i < SPI_PREAMBLE_BYTES; i++)
+	unsigned char check1Tx, check2Tx;
+	unsigned short int i;
+	unsigned short int sum1Tx, sum2Tx;
+
+	unsigned char high = 20, low = 40;
+
+	// The first four bytes are preamble bytes
+	for (i = 0; i < SPI_PREAMBLE_BYTES; i++)
+	{
+		p_motorCommand[i] = 0xFF;
+	}
+
+	sum1Tx = 0;
+	sum2Tx = 0;
+
+
+	for (i = SPI_PREAMBLE_BYTES; i < (SPI_PREAMBLE_BYTES+SPI_TX_DATA_SIZE); i++)
+	{
+		p_motorCommand[i] = i-SPI_PREAMBLE_BYTES;
+
+		if ((i == 4) | (i == 10) | (i == 16) | (i == 21) | (i == 26))
 		{
-			p_motorCommand[i] = 0xFF;
+			p_motorCommand[i] = high;
 		}
 
-		sum1Tx = 0;
-		sum2Tx = 0;
-
-		for (i = SPI_PREAMBLE_BYTES; i < (SPI_PREAMBLE_BYTES+SPI_TX_DATA_SIZE); i++)
+		else if ((i == 5) | (i == 11) | (i == 17) | (i == 22) | (i == 27))
 		{
-			p_motorCommand[i] = i-SPI_PREAMBLE_BYTES;
-
-			if ((i == 4) | (i == 10) | (i == 16) | (i == 21) | (i == 26))
-			{
-				p_motorCommand[i] = high;
-			}
-
-			else if ((i == 5) | (i == 11) | (i == 17) | (i == 22) | (i == 27))
-			{
-				p_motorCommand[i] = high;
-			}
-
-			else if ((i == 6) | (i == 12) | (i == 18) | (i == 23) | (i == 28))
-			{
-				p_motorCommand[i] = high;
-			}
-
-			else if ((i == 7) | (i == 13) | (i == 19) | (i == 24) | (i == 29))
-			{
-				p_motorCommand[i] = low;
-			}
-
-			else if ((i == 8) | (i == 14) | (i == 20) | (i == 25) | (i == 30))
-			{
-				p_motorCommand[i] = low;
-			}
-
-			else if ((i == 9) | (i == 15) | (i == 21) | (i == 26) | (i == 31))
-			{
-				p_motorCommand[i] = 0b10100;
-			}
-
-
-			sum1Tx = sum1Helper(sum1Tx, p_motorCommand[i]);
-			sum2Tx = sum2Helper(sum1Tx, sum1Tx);
+			p_motorCommand[i] = high;
 		}
 
-		check1Tx = check1Helper(sum1Tx, sum2Tx);
-		check2Tx = check2Helper(sum1Tx, check1Tx);
-
-		p_motorCommand[SPI_PREAMBLE_BYTES+SPI_TX_DATA_SIZE] = check1Tx;
-		p_motorCommand[SPI_PREAMBLE_BYTES+SPI_TX_DATA_SIZE+1] = check2Tx;
-
-		// Fill the rest with 0s...Tx and Rx data have to be the same length for SPI
-		for (i = (SPI_PREAMBLE_BYTES+SPI_TX_DATA_SIZE+SPI_CHECKSUM_SIZE); i < SPI_TRANSMISSION_SIZE; i++)
+		else if ((i == 6) | (i == 12) | (i == 18) | (i == 23) | (i == 28))
 		{
-			p_motorCommand[i] = 0x0;
+			p_motorCommand[i] = high;
 		}
+
+		else if ((i == 7) | (i == 13) | (i == 19) | (i == 24) | (i == 29))
+		{
+			p_motorCommand[i] = low;
+		}
+
+		else if ((i == 8) | (i == 14) | (i == 20) | (i == 25) | (i == 30))
+		{
+			p_motorCommand[i] = low;
+		}
+
+		else if ((i == 9) | (i == 15) | (i == 21) | (i == 26) | (i == 31))
+		{
+			p_motorCommand[i] = 0b10100;
+		}
+
+
+		sum1Tx = sum1Helper(sum1Tx, p_motorCommand[i]);
+		sum2Tx = sum2Helper(sum1Tx, sum1Tx);
+	}
+
+	check1Tx = check1Helper(sum1Tx, sum2Tx);
+	check2Tx = check2Helper(sum1Tx, check1Tx);
+
+	p_motorCommand[SPI_PREAMBLE_BYTES+SPI_TX_DATA_SIZE] = check1Tx;
+	p_motorCommand[SPI_PREAMBLE_BYTES+SPI_TX_DATA_SIZE+1] = check2Tx;
+
+	// Fill the rest with 0s...Tx and Rx data have to be the same length for SPI
+	for (i = (SPI_PREAMBLE_BYTES+SPI_TX_DATA_SIZE+SPI_CHECKSUM_SIZE); i < SPI_TRANSMISSION_SIZE; i++)
+	{
+		p_motorCommand[i] = 0x0;
+	}
 
 }
 
