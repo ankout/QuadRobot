ed548771019205e2e865293ad75cbeb7b7e41afa
acardi <acardi@debian.AAA>
2017-09-03 15:13:57 -0400

    Working on joint remapping

--- QuadRobotBBB/Include/jointConstants.h
+++ QuadRobotBBB/Include/jointConstants.h
@@ -38,7 +38,7 @@ const float encDir[20] = {
 #define TS 0.01f // sampling period in seconds
 
 const float P_GAIN[20] = {
-		1.0f, 1.0f, 1.0f, 0.01f, 0.01f, // Leg 1
+		1.0f, 1.0f, 1.0f, 0.0001f, 0.0001f, // Leg 1
 		1.0f, 1.0f, 1.0f, 1.0f, 1.0f, // Leg 2
 		1.0f, 1.0f, 1.0f, 1.0f, 1.0f, // Leg 3
 		1.0f, 1.0f, 1.0f, 1.0f, 1.0f  // Leg 4
@@ -57,5 +57,35 @@ const float D_GAIN[20] = {
 		0.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Leg 3
 		0.0f, 0.0f, 0.0f, 0.0f, 0.0f  // Leg 4
 };
+
+// There are 25 total motor channels and only 20 physical joints.
+// All of the motion planning and control loop calculations are done
+// assuming there are 20 motor channels and 20 joints, where:
+//
+// 		Leg 1: [0]-[4], where [0] is the first joint
+// 		Leg 2: [5]-[9], where [5] is the first joint
+// 		Leg 3: [10]-[14], where [10] is the first joint
+// 		Leg 4: [15]-[19], where [15] is the first joint
+//
+// In reality, the first joint in each leg has two motors and one of the LEG PCBs
+// only has 10A diode protection, so motors and channels need to get reassigned
+// before commands are sent out.  This is done at the very end after all calculations
+// to make code more maintainable.
+//
+// jointAssignments[] contains the mapping from the computed control actions (duty cycles)
+// from u[20] in robotmotion.cpp to 25 positions in motorCommand[]. The duty cycle
+// commands are not continuous in motorCommand[] like they are in u[] because motorCommand[]
+// interleaves direction commands after each group of five motor commands (one direction command per PCB)
+// If a number is repeated in jointAssignments[] that means that one joint is driven by two motors
+const unsigned char jointMapping[25] = {
+		0, 0, 1, 2, 3,
+		5, 5, 6, 7, 8,
+		10, 10, 11, 12, 13,
+		15, 15, 16, 17, 18,
+		4, 9, 14, 19, 19}; // The last entry doesn't matter because there is no motor on that channel
+
+
+#define U_MAX 10.0f
+
 
 #endif /* JOINTCONSTANTS_H_ */
--- QuadRobotBBB/src/robotMotion.cpp
+++ QuadRobotBBB/src/robotMotion.cpp
@@ -101,7 +101,7 @@ void getMotorCommands(unsigned char *p_motorCommand, float *p_desiredAngle, stru
 	static float uHist[NUM_ENCODERS*NUM_LEGS]; // contains previous control input
 	static float u[NUM_ENCODERS*NUM_LEGS]; // contains current control input
 
-	unsigned char OL, IL, curJoint, IIL;
+	unsigned char OL, IL, curJoint, IIL, switchWith;
 
 	unsigned char check1Tx, check2Tx;
 	unsigned short int i;
@@ -109,43 +109,126 @@ void getMotorCommands(unsigned char *p_motorCommand, float *p_desiredAngle, stru
 
 	unsigned char high = 20, low = 40;
 
+	sum1Tx = 0;
+	sum2Tx = 0;
+
 	// The first four bytes are preamble bytes
 	for (i = 0; i < SPI_PREAMBLE_BYTES; i++)
 	{
 		p_motorCommand[i] = 0xFF;
 	}
 
-//	for (OL = 0; OL < NUM_LEGS; OL++)
-//	{
-//		for (IL = 0; IL < NUM_ENCODERS; IL++)
-//		{
-//			curJoint = OL*NUM_ENCODERS+IL;
-//
-//			currentJointAngle[curJoint] =
-//					(float)(p_LEGdata[OL].encoder[IL] - minEncRead[curJoint])*DEG_PER_BIT*encDir[curJoint] + angleAtMinEncoderReading[curJoint];
-//			errorHist[curJoint] = p_desiredAngle[curJoint] - currentJointAngle[curJoint];
-//
-//			// Formula for control law from http://portal.ku.edu.tr/~cbasdogan/Courses/Robotics/projects/Discrete_PID.pdf
-//			u[curJoint] = uHist[curJoint] +
-//					(P_GAIN[curJoint] + I_GAIN[curJoint]*TS/2.0f + D_GAIN[curJoint]/TS)*errorHist[curJoint] +
-//					(-P_GAIN[curJoint] + I_GAIN[curJoint]*TS/2.0f - 2*D_GAIN[curJoint]/TS)*errorHist[curJoint+NUM_ENCODERS*NUM_LEGS] +
-//					(D_GAIN[curJoint]/TS)*errorHist[curJoint+NUM_ENCODERS*NUM_LEGS*2];
-//
-//			uHist[curJoint] = u[curJoint];
-//
-//			errorHist[curJoint+NUM_ENCODERS*NUM_LEGS*2] = errorHist[curJoint+NUM_ENCODERS*NUM_LEGS]; // error two samples ago (for next time gains are calculated)
-//			errorHist[curJoint+NUM_ENCODERS*NUM_LEGS] = errorHist[curJoint]; // error one sample ago (for next time gains are calculated)
-//		}
-//	}
+	for (OL = 0; OL < NUM_LEGS; OL++)
+	{
+		// These bits are assigned by ORing, so they need to be zeriod out beforehand
+		p_motorCommand[(OL+1)*NUM_ENCODERS+SPI_PREAMBLE_BYTES] = 0;
+
+		for (IL = 0; IL < NUM_ENCODERS; IL++)
+		{
+			curJoint = OL*NUM_ENCODERS+IL;
+
+			currentJointAngle[curJoint] =
+					(float)(p_LEGdata[OL].encoder[IL] - minEncRead[curJoint])*DEG_PER_BIT*encDir[curJoint] + angleAtMinEncoderReading[curJoint];
+			errorHist[curJoint] = p_desiredAngle[curJoint] - currentJointAngle[curJoint];
+
+			// Formula for control law from http://portal.ku.edu.tr/~cbasdogan/Courses/Robotics/projects/Discrete_PID.pdf
+			u[curJoint] = uHist[curJoint] +
+					(P_GAIN[curJoint] + I_GAIN[curJoint]*TS/2.0f + D_GAIN[curJoint]/TS)*errorHist[curJoint] +
+					(-P_GAIN[curJoint] + I_GAIN[curJoint]*TS/2.0f - 2*D_GAIN[curJoint]/TS)*errorHist[curJoint+NUM_ENCODERS*NUM_LEGS] +
+					(D_GAIN[curJoint]/TS)*errorHist[curJoint+NUM_ENCODERS*NUM_LEGS*2];
+
+			if (u[curJoint] > U_MAX)
+			{
+				u[curJoint] = U_MAX;
+			}
+
+			else if (u[curJoint] < -U_MAX)
+			{
+				u[curJoint] = -U_MAX;
+			}
+
+			uHist[curJoint] = u[curJoint];
+
+			errorHist[curJoint+NUM_ENCODERS*NUM_LEGS*2] = errorHist[curJoint+NUM_ENCODERS*NUM_LEGS]; // error two samples ago (for next time gains are calculated)
+			errorHist[curJoint+NUM_ENCODERS*NUM_LEGS] = errorHist[curJoint]; // error one sample ago (for next time gains are calculated)
+		}
+	}
+
+	for (OL = 0; OL < (NUM_ENCODERS*NUM_LEG_PCBS); OL++)
+	{
+		if (u[jointMapping[OL]] >= 0.0f)
+		{
+			// The math inside the indexer takes care of the preamble offset and the fact that every fifth entry is motor direction information
+			p_motorCommand[OL+(OL/NUM_ENCODERS)+SPI_PREAMBLE_BYTES] = (unsigned char)(uHist[curJoint] + 0.5f);
+
+			// Direction bit is "1" if positive
+			p_motorCommand[NUM_ENCODERS + (1+NUM_ENCODERS)*(OL/NUM_ENCODERS) + SPI_PREAMBLE_BYTES] = //left of here
+
+
+
+					//p_motorCommand[(OL+1)*NUM_ENCODERS+SPI_PREAMBLE_BYTES] | (0b1 << IL);
+
+		}
+
+		else
+		{
+			p_motorCommand[OL+(OL/NUM_ENCODERS)+SPI_PREAMBLE_BYTES] = (unsigned char)(-uHist[curJoint] + 0.5f);
+		}
+	}
+
+	// This byte is built up by ORing, so it needs to be zeroed out
+	p_motorCommand[31] = 0;
+
+	for (i = 26; i < 31; i++)
+	{
+		switch(i)
+		{
+			// "switchWith" is the motor number that needs to be swapped (0-based)
+			// "i" is the byte index (0-based)
+			// motor number 0 starts after SPI_PREAMBLE_BYTES
+
+			case 26:
+				switchWith = 12;
+			break;
+			case 27:
+				switchWith = 12;
+				break;
+			case 28:
+				switchWith = 12;
+				break;
+			case 29:
+				switchWith = 12;
+				break;
+			case 30:
+				switchWith = 12;
+				break;
+
+		}
+
+		p_motorCommand[i] = p_motorCommand[switchWith];
+		p_motorCommand[31] = p_motorCommand[31] | ((0b1 << (switchWith % NUM_ENCODERS)) & p_motorCommand
+
+		// Bytes 26-31 (0-based counting) pertain to PCB 5
+
+		p_motorCommand[27] = 0;
+		p_motorCommand[28] = 0;
+		p_motorCommand[29] = 0;
+		p_motorCommand[30] = 0;
+		p_motorCommand[31] = 0b00000;
+
+	////////////////////////////////
+
+	for (i = SPI_PREAMBLE_BYTES; i < (SPI_PREAMBLE_BYTES+SPI_TX_DATA_SIZE); i++)
+	{
+		sum1Tx = sum1Helper(sum1Tx, p_motorCommand[curJoint+SPI_PREAMBLE_BYTES]);
+		sum2Tx = sum2Helper(sum1Tx, sum1Tx);
+	}
+
+	check1Tx = check1Helper(sum1Tx, sum2Tx);
+	check2Tx = check2Helper(sum1Tx, check1Tx);
 
 	//printf("Angle %d) %f\n",4,currentJointAngle[3]);
 	//printf("Angle %d) %f\n",5,currentJointAngle[4]);
-
-
-
-	sum1Tx = 0;
-	sum2Tx = 0;
-
 
 	for (i = SPI_PREAMBLE_BYTES; i < (SPI_PREAMBLE_BYTES+SPI_TX_DATA_SIZE); i++)
 	{
